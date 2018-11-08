e9e2599e09c5107b0e82e844e76dac9983902333
Raymond Gigliotti <ray@firestitch.com>
2018-11-08 10:22:08 -0500

    FC-T148_Add_acronym_example_to_Badge_Component

--- demo/assets/components/icon-example/icon-example.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'icon-example',
-  templateUrl: 'icon-example.component.html'
-})
-export class IconExampleComponent {
-}
--- demo/assets/components/image-example/image-example.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'image-example',
-  templateUrl: 'image-example.component.html'
-})
-export class ImageExampleComponent {
-}
