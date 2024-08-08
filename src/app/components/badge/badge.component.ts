4faa1b1c96221f92d2840ee95aeab112ab9579f6
Raymond Gigliotti <ray@firestitch.com>
2024-08-08 07:29:06 -0400

    icon + image type logic

--- src/app/components/badge/badge.component.ts
+++ src/app/components/badge/badge.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, Input, OnChanges, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, OnChanges } from '@angular/core';
 
 
 @Component({
@@ -7,7 +7,7 @@ import { ChangeDetectionStrategy, Component, Input, OnChanges, OnInit } from '@a
   styleUrls: ['./badge.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class FsBadgeComponent implements OnInit, OnChanges {
+export class FsBadgeComponent implements OnChanges {
 
   @Input() public color;
   @Input() public text = '';
@@ -19,63 +19,65 @@ export class FsBadgeComponent implements OnInit, OnChanges {
   @Input() public iconSize;
   @Input() public iconSizePercent = .7;
   @Input() public iconColor: string;
-  @Input() public backgroundSize;
+  @Input() public backgroundSize: string;
 
-  public styles: any = {};
+  public styles: { [key: string]: string } = {};
+  public iconStyle: { [key: string]: string } = {};
   public type: string;
-  public iconStyle: any = {};
-
-  constructor() { }
-
-  public ngOnInit() { }
 
   public ngOnChanges(changes) {    
     this.styles = {};
     this.iconStyle = {};
 
+    if(this.image) {
+      this.type = 'image';
+      this._initImage();
+    } else if(this.icon) {
+      this.type = 'icon';
+      this._initIcon();
+    } else {
+      this.type = 'text';
+    }
+
     if (this.backgroundSize) {
       this.styles['background-size'] = this.backgroundSize;
     }
 
-    if(this.icon) {
-      this.type = 'icon';
-      
-      if(!this.iconSize && this.iconSizePercent) {
-        const diff = this.size * this.iconSizePercent;
-        this.iconSize =  diff / 24;
-      }
-
-      if (this.iconSize) {
-        this.iconStyle.transform = 'scale(' + this.iconSize + ')';
-      }
-
-      if (this.iconColor) {
-        this.iconStyle.color = this.iconColor;
-      }
-    } else {
-      this.type = this.image ? 'image' : 'text';
-    }
-
     if (this.color) {
-        this.styles['background-color'] = this.color;
+      this.styles['background-color'] = this.color;
     }
 
     if (this.size) {
-        this.styles.width = this.size + 'px';
-        this.styles['min-width'] = this.size + 'px';
+      this.styles.width = `${this.size}px`;
+      this.styles['min-width'] = `${this.size}px`;
 
-        if (this.shape === 'circle' || this.shape === 'square') {
-            this.styles.height = this.size + 'px';
-            this.styles['line-height'] = this.size + 'px';
-        }
+      if (this.shape === 'circle' || this.shape === 'square') {
+        this.styles.height = `${this.size}px`;
+        this.styles['line-height'] = `${this.size}px`;
+      }
 
-        if (this.text) {
-          this.styles['font-size'] = (this.size / 2.5) + 'px';
-        }
+      if (this.text) {
+        this.styles['font-size'] = `${this.size / 2.5}px`;
+      }
+    }
+  }
+
+  private _initImage(): void {
+    this.styles['background-image'] = `url(${this.image})`;
+  }
+
+  private _initIcon(): void {
+    if(!this.iconSize && this.iconSizePercent) {
+      const diff = this.size * this.iconSizePercent;
+      this.iconSize =  diff / 24;
     }
 
-    if (this.image) {
-        this.styles['background-image'] = 'url(' + this.image + ')';
+    if (this.iconSize) {
+      this.iconStyle.transform = `scale(${  this.iconSize  })`;
+    }
+
+    if (this.iconColor) {
+      this.iconStyle.color = this.iconColor;
     }
   }
 
